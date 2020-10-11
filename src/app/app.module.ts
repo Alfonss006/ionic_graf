a009a43135980142abf28006e5f1a7d3329d0e3c
Darwin Poblete <darwinalfonsop@gmail.com>
2020-10-10 23:56:21 -0300

    add login and firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,31 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBBewl0pyj4nxsF-NJ2ebtSujVBaKI8lCs",
+  authDomain: "ionicdatagraf.firebaseapp.com",
+  databaseURL: "https://ionicdatagraf.firebaseio.com",
+  storageBucket: "ionicdatagraf.appspot.com",
+  messagingSenderId: '806253469963'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
